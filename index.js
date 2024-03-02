9c207a1561a57458aa531d0b4d3ee712b2ad1ae8
rimasultana <rimasultana20225@gmail.com>
2024-03-02 18:52:51 +0600

    push

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const fruits = ["Mango", "Banana", "Orange", "Apple", "Labu"];
 // let size =fruits.length;
-document.getElementById("demo").innerHTML = fruits.join(" * ");
+document.getElementById("demo").innerHTML = fruits.push("kiwi");
+
+
+document.getElementById("demo").innerHTML = fruits;
